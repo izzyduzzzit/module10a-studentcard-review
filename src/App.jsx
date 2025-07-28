9f00f9d3b9df1a253b99e02d0175e468455f2031
Izarra Villareal <izarravill@gmail.com>
2025-07-27 22:42:19 -0700

    Added meaningful comments to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@
 
 // GitHub URL: https://github.com/izzyduzzzit/module10a-studentcard-review
 
+// Importing StudentCard component from StudentCard.jsx
 import StudentCard from './StudentCard';
 
+// Creating the main App parent component that will pass props down to the imported StudentCard component
 function App() {
   return (
     <div>
-      <StudentCard
-        name="Emily Carter"
-        major="Computer Science"
-        year="Sophomore"
-        bio="I enjoy building interactive web apps and working on open source projects in my spare time."
-        imageUrl="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?w=150&h=150&fit=crop&crop=face"
+      <StudentCard // This calls and renders the StudentCard child component and passes the following props to it:
+        name="Emily Carter" // This is the name prop passed to the child component
+        major="Computer Science" // This is the major prop passed to the child component
+        year="Sophomore" // This is the year prop passed to the child component
+        bio="I enjoy building interactive web apps and working on open source projects in my spare time." // This is the bio prop passed to the child component
+        imageUrl="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?w=150&h=150&fit=crop&crop=face" // This is the imageUrl prop passed to the child component
       />
     </div>
   );
 }
 
-export default App;
+export default App; // Exports the default App so that it can also be imported if necessary
